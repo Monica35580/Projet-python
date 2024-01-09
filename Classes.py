740322b3ad0f012454a9a96bb72cfc5f8dbe0053
Monica35580 <91956878+Monica35580@users.noreply.github.com>
2024-01-09 13:08:06 +0000

    Classes nécessaires

--- Classes.py
+++ Classes.py
@@ -1,3 +1,7 @@
+
+#from Classes import RedditDocument
+#from Classes import Document
+
 # =============== CLASSE DOCUMENT ===============
 class Document:
     # Initialisation des variables de la classe
@@ -20,7 +24,6 @@ class Document:
         return f"{self.titre}, par {self.auteur}"
 
 
-
 # =============== CLASSE AUTHOR ===============
 class Author:
     def __init__(self, name):
@@ -33,3 +36,40 @@ class Author:
         self.production.append(production)
     def __str__(self):
         return f"Auteur : {self.name}\t# productions : {self.ndoc}"
+
+# =============== RedditDocument =============== 
+'''class RedditDocument(Document):
+    def __init__(self,titre="", auteur="", date="", url="", texte="" ,Nb_Com=None):
+        super().__init__(titre,auteur,date,url,texte)
+        self.Nb_Com = Nb_Com
+
+    def getNb_Com(self):
+        return(self.Nb_Com)
+   
+    def setNb_Com(self, Nb_Com):
+        self.Nb_Com=Nb_Com
+
+    def __str__(self):
+        return f"{super().__str__()} Nombre de commentaires : {self.Nb_Com}"
+
+    def getType(self):
+        return("Reddit")
+    
+# =============== ArxivDocument ===============
+class ArxivDocument(Document):
+    def __init__(self, titre="", auteur="", date="", url="", texte="", co_auteurs=None):
+        super().__init__(titre, auteur, date, url, texte)
+        self.co_auteurs = co_auteurs if co_auteurs is not None else []
+
+    def get_co_auteurs(self):
+        return self.co_auteurs
+
+    def set_co_auteurs(self, co_auteurs):
+        self.co_auteurs = co_auteurs
+
+    def __str__(self):
+        co_auteurs_str = ", ".join(self.co_auteurs) if self.co_auteurs else "404 "
+        return f"{super().__str__()} co-auteurs : {co_auteurs_str}"
+    
+    def getType(self):
+        return("Arxiv")'''
