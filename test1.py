26c09a44da78084ee0f769601d76642b5670af09
lucilecpp <150041882+lucilecpp@users.noreply.github.com>
2024-01-09 23:02:46 +0100

    version finale

--- test1.py
+++ test1.py
@@ -1,20 +1,17 @@
 # =============== LISTE DES IMPORTATIONS ===============
 import re
-import requests
 import pickle
 import praw
 import datetime
 import pandas as pd
 import urllib, urllib.request
 import xmltodict
-from bs4 import BeautifulSoup
 from Classes import Document, Author
 from Corpus import Corpus
-from sklearn.feature_extraction.text import TfidfVectorizer
 from datetime import datetime
 import sys
 sys.stdout.reconfigure(encoding='utf-8') # Pour configurer la sortie des articles en UTF-8 Sinon une erreurs dans le terminal car il ne peut pas décoder
-
+import fonctions as f
 
 # =============== RECUPERER CONTENU ET TRAITEMENT DU DOCUMENT ===============
 ########### Pour Récupérer Reddit
@@ -129,47 +126,6 @@ def recuperer_contenu_web():
     print(f"Nombre d'articles récupérés: {len(docs)}")
     return docs, auteurs, dates
 # ==================================================================Fonction pour le traitement du corpus=========================
-def nettoyer_texte(texte):
-    # Retirer les balises HTML
-    #texte_sans_html = BeautifulSoup(texte, 'html.parser').get_text()
-
-    # Imprimer le texte après suppression des balises HTML
-    #print("Texte sans HTML :", texte_sans_html)
-
-    # Supprimer les caractères spéciaux et les chiffres
-    texte_sans_html = re.sub(r'[^a-zA-Z\s.]+', '', texte)    
-    texte_propre = texte_sans_html.lower()
-    texte_propre = texte_propre.replace('\n', ' ')
-
-    # Imprimer le texte après suppression des caractères spéciaux et chiffres
-    #print("Texte après suppression des caractères spéciaux :", texte_sans_html)
-    '''
-    # Diviser le texte en mots en utilisant plusieurs délimitations
-    mots = re.split(r'\s+|[.,;\'"()]+', texte_propre)
-    # Imprimer le texte après suppression des caractères spéciaux et chiffres
-    #print("Texte après suppression des caractères spéciaux :", texte_sans_html)
-
-    # Diviser le texte en mots en utilisant plusieurs délimitations
-    mots = re.split(r'\s+|[.,;\'"()]+', texte_sans_html)
-
-    # Imprimer les mots après la division du texte
-    #print("Mots après division du texte :", mots)
-
-    # Filtrer les mots vides
-    mots_filtrés = [mot for mot in mots if mot]
-
-    # Imprimer les mots filtrés
-    #print("Mots filtrés :", mots_filtrés)
-
-    # Ajouter les mots à l'ensemble
-    vocabulaire_set = set(mots_filtrés)
-
-    # Construire un dictionnaire de vocabulaire avec des fréquences initiales à zéro
-    vocabulaire = {mot: 0 for mot in vocabulaire_set}
-
-    for mot in mots_filtrés:
-        vocabulaire[mot] += 1'''
-    return texte_propre
 
 # Pour avoir la date au bon format
 def formate_date(date_utc):
@@ -190,16 +146,16 @@ def extraire_texte_pertinent(contenu_web, termes_recherche):
                 for sub_element in element:
                     if isinstance(sub_element, str):
                         print("str")
-                        texte_nettoye = nettoyer_texte(sub_element)
+                        texte_nettoye = f.nettoyer_texte(sub_element)
                         if texte_nettoye and texte_nettoye not in texte_pertinent:
                             texte_pertinent.append(texte_nettoye) 
             elif isinstance(element, str):
-                texte_nettoye = nettoyer_texte(element)
+                texte_nettoye = f.nettoyer_texte(element)
                 if texte_nettoye and texte_nettoye not in texte_pertinent:
                     texte_pertinent.append(texte_nettoye)
     else :
         # Traitement si contenu_web est une chaîne de caractères (str)
-        texte_nettoye = nettoyer_texte(contenu_web)
+        texte_nettoye = f.nettoyer_texte(contenu_web)
         if texte_nettoye and texte_nettoye not in texte_pertinent:
             texte_pertinent.append(texte_nettoye)
 
@@ -283,25 +239,6 @@ def sauvegarder_et_charger_corpus(corpus):
         nouveau_corpus = pickle.load(f)
     return nouveau_corpus
 
-# =============== OBTENIR MATRICE TF-IDF ===============
-def obtenir_matrice_tfidf(textes):
-    # Imprimer les textes avant la vectorisation
-    # print("Textes pour la matrice TF-IDF :")
-    #for i, texte in enumerate(textes):
-    #    print(f"Document {i} : {texte}")
-
-    # Si le texte est de type
-    if isinstance(textes, str):
-        # On convertit en liste
-        textes = [textes]
-
-
-    # Utiliser TfidfVectorizer avec un seuil minimum de documents
-    vectoriseur = TfidfVectorizer(min_df=1)  # Réglez min_df en fonction de vos besoins
-    matrice_tfidf = vectoriseur.fit_transform(textes)
-    return matrice_tfidf
-
-
 # =============== Calculs pour comparer les textes ===============
 def comparer_nombres_mots_longueur(texte):
     # Si le texte est vide, renvoyer (0, 0)
@@ -360,81 +297,26 @@ contenu_web_reddit, auteurs_red, dates_red = obtenir_contenu_web()
 #print(contenu_web_reddit)
 #print(contenu_web_arxiv)
 
-#========================================================== Obtenir Dataframe ======================================
+# ========================================================== Obtenir Dataframe ======================================
 def creation_df(contenu, auteurs, dates, nom_var):
     contenu_propre=[]
     # Nettoyer le texte
     for i in contenu:
         # Application de la fonction nettoyer
-        resultat_nettoye = nettoyer_texte(i)
+        resultat_nettoye = f.nettoyer_texte(i)
 
         # Ajout du contenu nettoyer 
         contenu_propre.append(resultat_nettoye)
 
     # Création d'un DataFrame à partir des données récupérées
     df = pd.DataFrame({'Contenu': contenu, 'Auteur': auteurs, 'Date': dates})
+
     # Ajout d'une variable pour connaître l'origine du texte
     df['Origine'] = nom_var
+
     return df
 
-
-
-
-
-
-#========================================================== Reddit ====================================================
-'''contenu_propre=[]
-# Nettoyer le texte
-for i in contenu_web_reddit:
-    # Application de la fonction nettoyer
-    resultat_nettoye = nettoyer_texte(i)
-
-    # Ajout du contenu nettoyer 
-    contenu_propre.append(resultat_nettoye)
-
-# Comparer le nombre de mots et la longueur du document
-resultat_nettoye_str = ' '.join(contenu_web_reddit)
-nombre_mots, longueur_texte = comparer_nombres_mots_longueur(resultat_nettoye_str)
-print(f"Nombre de mots : {nombre_mots}")
-print(f"Longueur du texte : {longueur_texte} caracteres")
-
-# Récupérer le texte pertinent
-termes_recherche = ["olympic", "game"]
-#texte_pertinent = extraire_texte_pertinent(contenu_propre, termes_recherche)
-
-# Affichage des résultats
-#print("Texte pertinent sans duplication:", texte_pertinent)
-
-# Traiter le texte pertinent extrait
-longue_chaine_de_caracteres = traiter_texte_pertinent(contenu_propre)
-#print(stat(contenu_propre))
-
-# Utilisation de la fonction avec le texte pertinent traité
-matrice_tfidf = obtenir_matrice_tfidf(contenu_propre)
-#print(matrice_tfidf)
-
-# Création d'un DataFrame à partir des données récupérées
-df = pd.DataFrame({'Contenu': contenu_web_reddit, 'Auteur': auteurs_red, 'Date': dates_red})
-
-
-# Ajout de colonne pour le df pour compter les phrases, les mots et les caractères
-df['Origine'] = 'Reddit'
-#df['Nombre de Phrases'] = df['Contenu'].apply(compter_phrases)
-#df['Nombre de Mots'] = df['Contenu'].apply(compter_mots)
-#df['Nombre de Caracteres'] = df['Contenu'].apply(compter_caracteres)
-
-
-# Afficher le DataFrame
-pd.set_option('display.max_columns', None)
-pd.set_option('display.expand_frame_repr', False)
-#print(df)
-
-#Convertir le df en csv
-#df.to_csv('corpus.csv', index=False, sep='\t')
-'''
-
-
-########################################################### Arxiv #########################################################
+########################################################### Creation du corpus #########################################################
 
 # Création du corpus pour Arxiv
 arxiv=creation_df(contenu_web_arxiv, auteurs_ar, dates_ar, 'Arxiv')
@@ -453,12 +335,8 @@ df['Nombre_phrases'] = df['Contenu'].apply(compter_phrases)
 df['Nombre_mots'] = df['Contenu'].apply(compter_mots)
 df['Nombre_caracteres'] = df['Contenu'].apply(compter_caracteres)
 df['Comparer_mot']=df['Contenu'].apply(comparer_nombres_mots_longueur)
-df['Matrice tf-idf']=df['Contenu'].apply(obtenir_matrice_tfidf)
+df['Matrice tf-idf']=df['Contenu'].apply(f.obtenir_matrice_tfidf)
 
-
-# Afficher le DataFrame combiné
-#pd.set_option('display.max_columns', None)
-#pd.set_option('display.expand_frame_repr', False)
 print(df)
 
 ## Calcul matrice tf-idf
@@ -466,7 +344,7 @@ print(df)
 textes_colonne = df['Contenu']
 
 # Application de la fonction
-matrice_tfidf_resultat = obtenir_matrice_tfidf(textes_colonne)
+matrice_tfidf_resultat = f.obtenir_matrice_tfidf(textes_colonne)
 
 # Affichage des résultats
 print(matrice_tfidf_resultat)
@@ -481,58 +359,4 @@ corpus_liste=creer_document()
 #for document in corpus_liste:
 #    print(Document.__repr__(document))
 
-
-
-
-'''# Création d'un corpus à partir de la liste de documents
-corpus = creer_corpus(corpus_liste)
-
-# Afficher les documents dans le corpus
-corpus.show(n_docs=5, tri="abc")
-
-# Sauvegarder et charger le corpus
-nouveau_corpus = sauvegarder_et_charger_corpus(corpus)'''
-
-
-
-
-
-
-
-
-
-
-
-'''contenu_propre_ar=[]
-#nettoyer le texte
-for i in contenu_web_arxiv:
-    resultat_nettoye = nettoyer_texte(i)
-    contenu_propre_ar.append(resultat_nettoye)
-
-
-# Comparer le nombre de mots et la longueur du document
-resultat_nettoye_str = ' '.join(contenu_web_arxiv)
-nombre_mots, longueur_texte = comparer_nombres_mots_longueur(resultat_nettoye_str)
-print(f"Nombre de mots : {nombre_mots}")
-print(f"Longueur du texte : {longueur_texte} caracteres")
-
-# Récupérer le texte pertinent
-termes_recherche = ["olympic", "game"]
-texte_pertinent_ar = extraire_texte_pertinent(contenu_propre_ar, termes_recherche)
-
-# Affichage des résultats
-#print("Texte pertinent sans duplication:", texte_pertinent)
-
-# Traiter le texte pertinent extrait
-longue_chaine_de_caracteres = traiter_texte_pertinent(texte_pertinent_ar)
-
-# Utilisation de la fonction avec le texte pertinent traité
-matrice_tfidf = obtenir_matrice_tfidf(texte_pertinent_ar)
-print(matrice_tfidf)
-
-# créer un document
-liste_documents = []
-
-# Ajout du document à la liste
-liste_documents.append(Document())'''
 
